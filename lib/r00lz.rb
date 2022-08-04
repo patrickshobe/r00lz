566961529e0eb521a9125b035aee37b3ad24fe1e
Patrick Shobe <patrick.shobe@housecallpro.com>
2022-08-04 14:34:35 -0600

    add constant autoloading

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -1,12 +1,46 @@
+# frozen_string_literal: true
+
 require 'r00lz/version'
 
 module R00lz
+  class Object
+    def self.const_missing(const)
+      require R00lz.to_underscore(const.to_s)
+      Object.const_get(const)
+    end
+  end
+
   class Error < StandardError; end
+
+  def self.to_underscore(string)
+    string.gsub(
+      /([A-Z]+)([A-Z][a-z])/,
+      '\1_\2'
+    ).gsub(
+      /([a-z\d])([A-Z])/,
+      '\1_\2'
+    ).downcase
+  end
+
   class App
     def call(env)
-      [200,
-       { 'Content-Type' => 'text/html' },
-       ['Hello from r00lz!']]
+      klass, action = cont_and_act(env)
+      text = klass.new(env).send(action)
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+
+    def cont_and_act(env)
+      _, controller, action, _after = env['PATH_INFO'].split('/')
+      controller = "#{controller.capitalize}Controller"
+      [Object.const_get(controller), action]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
--- test/r00lz_test.rb
+++ test/r00lz_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
+class TedController < R00lz::Controller
+  def think
+    'Whoah, man...'
+  end
+end
+
 class R00lzTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::R00lz::VERSION
   end
 
   def test_app_return_success
-    env = { 'PATH_INFO' => '/',
+    env = { 'PATH_INFO' => '/ted/think',
             'QUERY_STRING' => '' }
     assert_equal 200, ::R00lz::App.new.call(env)[0]
   end
